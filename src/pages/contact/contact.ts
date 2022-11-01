1b65244d9eaab758fa812b267e176995e25de34a
s64309010032 <s64309010032@ptnicec.ac.th>
2022-11-01 17:29:22 +0700

    ส่งครั้งที่ 1

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -11,4 +11,18 @@ export class ContactPage {
 
   }
 
+  // Doughnut
+public doughnutChartLabels:string[] = ['ปวช.1', 'ปวช.2', 'ปวช.3', 'ปวส.1', 'ปวส.2'];
+public doughnutChartData:number[] = [350, 450, 100, 45, 39];
+public doughnutChartType:string = 'doughnut';
+
+// events
+public chartClicked(e:any):void {
+  console.log(e);
+}
+
+public chartHovered(e:any):void {
+  console.log(e);
+}
+
 }
